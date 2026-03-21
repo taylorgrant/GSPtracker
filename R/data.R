5c574db812688b3b03d0f0314b495eec2b66c98f
taylorgrant <taylorgrant38@gmail.com>
2026-03-20 18:03:12 -0700

    benchmark data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Creative benchmarks for 2025
+#'
+#' Data frames containing 2025 creative benchmark values used in the brand
+#' tracker. Benchark values are split out by "Social" and "Other".
+#'
+#' Data frames can be found for the Overall audience, Women, AAPI, and Gen Z/Millennials.
+#'
+#' @format A data frame.
+#' @source Internal benchmark workbook.
+"creative_benchmarks_2025"
