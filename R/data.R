97f586bccff60b3377cab52d8a94fce2487be613
taylorgrant <taylorgrant38@gmail.com>
2026-03-20 19:04:36 -0700

    benchmark datasets

--- R/data.R
+++ R/data.R
@@ -8,3 +8,23 @@
 #' @format A data frame.
 #' @source Internal benchmark workbook.
 "creative_benchmarks_2025"
+
+#' BMW brand benchmarks for 2025
+#'
+#' Data frames containing 2025 benchmarks for all metrics, attributes, and traits for BMW
+#'
+#' Data frames can be found for the Overall audience, Women, AAPI, and Gen Z/Millennials.
+#'
+#' @format A data frame.
+#' @source Internal benchmark workbook.
+"bmw_benchmarks_2025"
+
+#' BMW brand benchmarks for 2025 split out by quarter
+#'
+#' Data frames containing quarterly 2025 benchmarks for all metrics, attributes, and traits for BMW
+#'
+#' Data frames can be found for the Overall audience, Women, AAPI, and Gen Z/Millennials.
+#'
+#' @format A data frame.
+#' @source Internal benchmark workbook.
+"bmw_qtrly_benchmarks_2025"
--- data-raw/benchmark_data.R
+++ data-raw/benchmark_data.R
@@ -18,6 +18,50 @@ creative_benchmarks_2025 <- list(
     "data-raw/benchmarks_2025.xlsx",
     sheet = "genz_mill"
   )
-) ## code to prepare `DATASET` dataset goes here
+)
 
 usethis::use_data(creative_benchmarks_2025, overwrite = TRUE)
+
+# BMW benchmarks 2025
+bmw_benchmarks_2025 <- list(
+  Overall = readxl::read_excel(
+    "data-raw/bmw_benchmarks_2025.xlsx",
+    sheet = "bmw_overall"
+  ),
+  Women = readxl::read_excel(
+    "data-raw/bmw_benchmarks_2025.xlsx",
+    sheet = "bmw_female"
+  ),
+  AAPI = readxl::read_excel(
+    "data-raw/bmw_benchmarks_2025.xlsx",
+    sheet = "bmw_aapi"
+  ),
+  GenZMill = readxl::read_excel(
+    "data-raw/bmw_benchmarks_2025.xlsx",
+    sheet = "bmw_zm"
+  )
+)
+
+usethis::use_data(bmw_benchmarks_2025, overwrite = TRUE)
+
+# BMW Quarterly Benchmarks
+bmw_qtrly_benchmarks_2025 <- list(
+  Overall = readxl::read_excel(
+    "data-raw/bmw_qtrly_benchmarks_2025.xlsx",
+    sheet = "bmw_overallq"
+  ),
+  Women = readxl::read_excel(
+    "data-raw/bmw_qtrly_benchmarks_2025.xlsx",
+    sheet = "bmw_femaleq"
+  ),
+  AAPI = readxl::read_excel(
+    "data-raw/bmw_qtrly_benchmarks_2025.xlsx",
+    sheet = "bmw_aapiq"
+  ),
+  GenZMill = readxl::read_excel(
+    "data-raw/bmw_qtrly_benchmarks_2025.xlsx",
+    sheet = "bmw_zmq"
+  )
+)
+
+usethis::use_data(bmw_qtrly_benchmarks_2025, overwrite = TRUE)
